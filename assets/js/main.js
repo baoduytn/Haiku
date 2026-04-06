345e59ba0e627626362044fd84c0924478809e95
baoduytn <baoduy1028@gmail.com>
2026-04-06 00:24:58 +0000

    more changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,5 +15,17 @@ document.addEventListener("click", function () {
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + 1) + min;
 }
+let divs = document.querySelectorAll(".grid-container div");
+function randomize() {
+    console.log(divs.length, "randomize!")
+} 
+
+/* let colors = ["purple", "green", "blue"];
+colors[randomNumber(0,colors.length-1)]
+
+ElementInternals.forEach(function(ele){
+    ele.style.transform = 'rotate(${Math.random() * 360}deg)';
+}); */
+
 
 console.log("Hello, World!");
