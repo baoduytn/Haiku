b1d42e4a6f9a8a1902b46d0137e90747d6b3e85e
baoduytn <baoduy1028@gmail.com>
2026-04-06 03:57:38 +0000

    more changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,32 @@
 // main.js
-/* document.body.style.opacity = .5;
-document.body.style.transform = 'rotate(90deg)';
-*/
-
-let rotation = 0;
-document.addEventListener("click", function () {
-    // let min = 10;
-    // let max = 40;
-    // rotation += Math.floor(Math.random() * (max - min + 1)) + min;
-    rotation += randomNumber(1,200);
-    document.body.style.transform = `rotate(${rotation}deg)`;
-    });
-
-function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + 1) + min;
-}
+let grid = document.querySelectorAll(".grid-container");
 let divs = document.querySelectorAll(".grid-container div");
+
 function randomize() {
     console.log(divs.length, "randomize!")
-    divs.forEach(function(div) {
+
+    // affect the whole grid
+    grid[0].style.transform = `rotate(${Math.random() * 360}deg)`;
+
+    // just the grid divs
+    divs.forEach(function (div) {
         let scale = randomNumber(.5, 1.2);
         let translateX = randomNumber(0, 50);
         let translateY = randomNumber(0, 20);
         let rotate = randomNumber(0, 360);
-        div.style.transform = 
-           ' scale(${scale})
-            translateX(${translateX}%, ${translateY}%)
-            rotate(${rotate}deg)';) 
-})
 
-/* let colors = ["purple", "green", "blue"];
-colors[randomNumber(0,colors.length-1)]
+        // 50% of the time ...
+        if (Math.random() > .5) {
+            scale = 1; // don't affect scale
+            translateX = randomNumber(0, 80);
+            translateY = randomNumber(0, 80);
+        }
 
-ElementInternals.forEach(function(ele){
-    ele.style.transform = 'rotate(${Math.random() * 360}deg)';
-}); */
+        div.style.transform = `scale(${scale}) translate(${translateX}%, ${translateY}%) rotate(${rotate}deg)`;
+    });
+}
+document.addEventListener("click", randomize);
 
-
-console.log("Hello, World!");
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
