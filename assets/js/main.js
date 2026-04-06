87707acd97cf82d9e5ad70b574e9848ee66f5c21
baoduytn <baoduy1028@gmail.com>
2026-04-06 00:09:45 +0000

    more changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
 // main.js
+/* document.body.style.opacity = .5;
+document.body.style.transform = 'rotate(90deg)';
+*/
+
+let rotation = 0;
+document.addEventListener("click", function () {
+    // let min = 10;
+    // let max = 40;
+    // rotation += Math.floor(Math.random() * (max - min + 1)) + min;
+    rotation += randomNumber(1,200);
+    document.body.style.transform = `rotate(${rotation}deg)`;
+    });
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) + 1) + min;
+}
 
 console.log("Hello, World!");
